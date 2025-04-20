1fcf4b8fe30a85cac45b9caadf5044a1d7c586db
PracticalSecret <murigipeninah1@gmail.com>
2025-04-20 20:01:05 +0300

    ownership in RUST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     let s1 = String::from("RUST");
+    let len = calculate_length(&s1);
+    println!("Length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize{
+    s.len()
 }
